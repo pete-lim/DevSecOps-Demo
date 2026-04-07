afabdcc36ed44ea06a82a7d78860b8c52cde41c1
Pete <petedaniel.lim@gmail.com>
2026-04-07 14:59:17 +0930

    Kubernetes pipeline and cluster deployments

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello DevSecOps');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello DevSecOps');
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
